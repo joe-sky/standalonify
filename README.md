34f916ba393660f3116544a9cc74f30df7f5f845
joe-sky <hitomi_doax@sina.com>
2016-05-22 16:48:05 +0800

    Commit v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # standalonify
 A Browserify plugin to make standalone UMD bundles.
+
+## Installation
+npm install -save standalonify
+
+## Usage
+```js
+var browserify = require('browserify');
+var standalonify = require('standalonify');
+
+browserify({
+  entries: 'your entry file'
+}).plugin(standalonify, {
+  name: 'moduleName',  //or set such as "['moduleName1', 'moduleName2']", can set more than one module name.
+  deps: {
+    'react': 'React',  // require('react') will use AMD's and CommonJS's require('react') or the React global object.
+    'react-dom': 'ReactDOM'
+  }
+});
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+var through = require('through2'),
+    format = require('string-format-js');
+
+var prefix = "(function(_g) {(function(f) { var r = (typeof require === 'function' ? require : function(name) { return #{depsMap}[name]; }); if (typeof exports === 'object' && typeof module !== 'undefined') { module.exports = f(r) } else if (typeof define === 'function' && define.amd) { define(#{depsKeys}, f.bind(_g,r)) } else { f(r) } })(function(require,define, module,exports) { var _m = ";
+
+var suffix = "var _r = _m(#{moduleKey});#{globalDefine}_r;return _r;})})(typeof window!=='undefined' ? window : (typeof global!=='undefined' ? global : (typeof self!=='undefined' ? self : this)));";
+
+function createStream(prefix, suffix) {
+  var first = true;
+
+  return through(function (chunk, encoding, next) {
+    if (first) {
+      this.push(new Buffer(prefix()));
+      first = false;
+    }
+    this.push(chunk);
+    next();
+  }, function (next) {
+    this.push(new Buffer(suffix()));
+    next();
+  });
+}
+
+module.exports = function (b, opts) {
+  if (!opts) {
+    throw new Error('Please provide some options to the plugin');
+  } else if (opts.name == null) {
+    throw new Error('Please specifiy a name for the module');
+  }
+
+  var deps = opts.deps || {},
+      keys = Object.keys(deps);
+
+  function applyPlugin() {
+    return b.external(keys)
+        .pipeline.get('wrap')
+        .push(createStream(function () {
+          return prefix.format({
+            depsKeys: JSON.stringify(keys),
+            depsMap: '{' + keys.map(function (key) {
+              return JSON.stringify(key) + ":" + deps[key];
+            }).join(',') + '}'
+          });
+        }, function () {
+          return suffix.format({
+            // This is pretty hacky but Browserify has the standalone mode internalized
+            moduleKey: b._bpack.standaloneModule,
+            globalDefine: (Array.isArray(opts.name) ? opts.name : [opts.name]).map(function (name) {
+              return '_g.' + name + ' = ';
+            }).join('')
+          });
+        }));
+  }
+
+  b.on('reset', applyPlugin);
+
+  return applyPlugin();
+};
